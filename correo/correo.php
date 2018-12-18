1b05b45c53bed8ce2a6236084c129226c51c4522
yadersystem <jader_16@hotmail.com>
2018-12-18 14:09:15 -0500

    correcion del formulario de Correo ademas de su configuracion prueba 1

---
+++ correo/correo.php
@@ -0,0 +1,12 @@
+<?php 
+ 	$nombre=$_POST['nombre'];
+ 	$correo=$_POST['correo'];
+	$mensaje=$_POST['mensaje'];
+
+	if (mail('sistemasjader32@gmail.com',$nombre,$correo,$mensaje)) {
+		echo "Mensaje Enviado";
+	} else {
+		echo "Error al enviar mensaje";
+	}
+	
+ ?>
